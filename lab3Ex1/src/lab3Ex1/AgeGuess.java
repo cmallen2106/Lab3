83f9218c8f4907bf3729fd4b1fb8a7fdabe2e551
cmallen2106 <cmallen2106@gmail.com>
2021-07-16 22:24:04 -0600

    First Commit

---
+++ lab3Ex1/src/lab3Ex1/AgeGuess.java
@@ -0,0 +1,9 @@
+package lab3Ex1;
+
+public class AgeGuess {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ lab3Ex2/src/lab3Ex2/EvenNumSum.java
@@ -0,0 +1,9 @@
+package lab3Ex2;
+
+public class EvenNumSum {
+
+	public static void main(String[] args) {
+
+	}
+
+}
