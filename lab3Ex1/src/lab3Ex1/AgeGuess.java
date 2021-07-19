de03370f08126b1a580e26356594c3bae445a965
cmallen2106 <cmallen2106@gmail.com>
2021-07-19 16:41:53 -0600

    copy AgeGuess program from Lab2

--- lab3Ex1/src/lab3Ex1/AgeGuess.java
+++ lab3Ex1/src/lab3Ex1/AgeGuess.java
@@ -1,9 +1,33 @@
+//Lab 3 - Exercise 1
+//Christina Sosa
+//Programming Fundamentals, Summer 2021
 package lab3Ex1;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class AgeGuess {
-
-	public static void main(String[] args) {
-
+	public static void main(String[]args) {
+		
+		Random randNum = new Random();
+		
+		//Declare a new int variable age &
+		//Initialize age to a random integer between 0 and 100 (inclusive)
+		int age = randNum.nextInt(100); 
+		
+		try (Scanner scan = new Scanner (System.in)) {
+			int ageGuess; 
+			
+			//Asks the user for a guess, save the guess into the ageGuess variable
+			System.out.println("Guess the age:"); 
+			ageGuess = scan.nextInt(); 
+			
+			
+			//Display the user guess and the correct answer
+			System.out.println("The age you guessed is " + ageGuess + ", and the correct age is " + age + "!");
+			
+			scan.close(); 
+		}
+		
 	}
-
 }
