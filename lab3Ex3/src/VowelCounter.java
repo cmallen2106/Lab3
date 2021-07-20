0e76ef0504fdfb019df85106985d6db0ec935f82
cmallen2106 <cmallen2106@gmail.com>
2021-07-20 17:34:24 -0600

    scan for vowels & non-vowels

--- lab3Ex3/src/VowelCounter.java
+++ lab3Ex3/src/VowelCounter.java
@@ -10,13 +10,55 @@ public class VowelCounter {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in); 
+		
 		int countA = 0; 
 		int countE = 0; 
+		int countI = 0; 
+		int countO = 0; 
+		int countU = 0; 
+		int nonVowel = 0;  
 		
+		//ask user for a string
 		System.out.println("Please enter any word or sentence: "); 
-		Scanner scan;
-		String userString = scan.nextLine(); 
-
+		String line = scan.nextLine(); 
+		
+		line = line.toLowerCase();
+		
+		//scan string for vowels and non-vowels
+		for (int index = 0; index < line.length(); index++) {
+			
+			char userChar = line.charAt(index); 
+			if (userChar == 'a') {
+				countA++; 
+			}
+			else if (userChar == 'e') {
+				countE++; 
+			}
+			else if (userChar == 'i') {
+				countI++; 
+			}
+			else if (userChar == 'o') {
+				countO++; 
+			}
+			else if (userChar == 'u') {
+				countU++; 
+			}
+			else if ((userChar >= 'a' && userChar <= 'z')) {
+				nonVowel++; 
+			}
+			
+			
+		}
+		
+		//print out number of vowels and non-vowels
+		System.out.println("There are " + countA + " lowercase a characters.");
+		System.out.println("There are " + countE + " lowercase e characters.");
+		System.out.println("There are " + countI + " lowercase i characters.");
+		System.out.println("There are " + countO + " lowercase o characters.");
+		System.out.println("There are " + countU + " lowercase u characters.");
+		System.out.println("There are " + nonVowel + " non-vowel characters.");
+		
+		//close scanner
 		scan.close();
 	}
 
