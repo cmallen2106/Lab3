ce7370e5b3433e66d0fb0259734d884206a21fcb
cmallen2106 <cmallen2106@gmail.com>
2021-07-19 21:20:00 -0600

    create userInt int

--- lab3Ex2/src/lab3Ex2/EvenNumSum.java
+++ lab3Ex2/src/lab3Ex2/EvenNumSum.java
@@ -1,9 +1,22 @@
+//Lab 3 - Exercise 2
+//Christina Sosa
+//Programming Fundamentals, Summer 2021
+
 package lab3Ex2;
+import java.util.Scanner; 
 
 public class EvenNumSum {
 
 	public static void main(String[] args) {
 
+		Scanner scan = new Scanner (System.in); 
+		
+		int userInt; 
+		
+		System.out.println("Please enter an integer greater than 2: "); 
+		userInt = scan.nextInt();
+		
+		
 	}
 
 }
