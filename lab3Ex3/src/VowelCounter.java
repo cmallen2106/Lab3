2019b9e2535c164c38e676c810f2f5cef8e0d2c5
cmallen2106 <cmallen2106@gmail.com>
2021-07-20 14:31:40 -0600

    created A & E int, asked user to enter word or sentence

--- lab3Ex3/src/VowelCounter.java
+++ lab3Ex3/src/VowelCounter.java
@@ -1,8 +1,23 @@
+//Lab 3 - Exercise 3
+//Christina Sosa
+//Programming Fundamentals, Summer 2021
+
+import java.util.Scanner; 
+import java.lang.String; 
 
 public class VowelCounter {
 
 	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in); 
+		int countA = 0; 
+		int countE = 0; 
+		
+		System.out.println("Please enter any word or sentence: "); 
+		Scanner scan;
+		String userString = scan.nextLine(); 
 
+		scan.close();
 	}
 
 }
