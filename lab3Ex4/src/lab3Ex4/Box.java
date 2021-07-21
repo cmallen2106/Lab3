beceea911997c81d2bd0217a2c9ba0ea49d8d518
cmallen2106 <cmallen2106@gmail.com>
2021-07-20 18:01:38 -0600

    create box object and method to set size of printBox

--- lab3Ex4/src/lab3Ex4/Box.java
+++ lab3Ex4/src/lab3Ex4/Box.java
@@ -1,9 +1,26 @@
+//Lab 3 - Exercise 4
+//Christina Sosa
+//Programming Fundamentals, Summer 2021
+
 package lab3Ex4;
 
 public class Box {
+	
+	int size = 0; 
 
 	public static void main(String[] args) {
+		Box box = new Box(); 
+		box.setSize(); 
+		 
 
 	}
+	
+	//method to set size of printBox method
+	public void setSize() {
+		size = 5; 
+	}
+	
+	
+		 
 
 }
